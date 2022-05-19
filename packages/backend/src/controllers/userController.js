9cfa851068187de1959b06bf926eecd6979c494f
miguelgc95 <miguelgc95@gmail.com>
2022-05-19 13:27:14 +0200

    working as before but in monorepo

--- packages/backend/src/controllers/userController.js
+++ packages/backend/src/controllers/userController.js
@@ -2,11 +2,8 @@ import UserModel from '../models/UserModel.js'
 
 async function getUser(req, res) {
 	const mail = 'mike@test.com'
-	// const userMail = req.user.mail;
 	try {
 		const response = await UserModel.findOne({ mail }).lean().exec()
-		// const response = 'q dise maik';
-		console.log(response)
 
 		if (response) return res.status(200).send(response)
 		return res.status(400).send('error')
